e1f292d4cd6d654333f4c5f2cb5224515d62d125
Miguel Ángel Ríos <ro.miguelangel@gmail.com>
2014-01-22 13:41:45 +0100

    Change rename app kernel mmoreram bundles to payment suite

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Mmoreram\FrontBundle\MmoreramFrontBundle(),
-            new Mmoreram\PaymentBridgeBundle\PaymentBridgeBundle(),
+            new PaymentSuite\FrontBundle\PaymentSuiteFrontBundle(),
+            new PaymentSuite\PaymentBridgeBundle\PaymentBridgeBundle(),
 
             new PaymentSuite\PaymentCoreBundle\PaymentCoreBundle(),
             new PaymentSuite\PaymillBundle\PaymillBundle(),
